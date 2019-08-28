710584810e66b10ddc22d4fa0110a78cc0fd8252
LeakyBucket <DamnBigMan@gmail.com>
2019-08-27 20:04:49 -0600

    So many things…

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+pub mod template;
+
 use std::env;
+use std::collections::HashMap;
 
 use tokio::runtime::Runtime;
 
-use hubcaps::issues::IssueOptions;
 use hubcaps::{Credentials, Github, Result};
-
-use hyper::client::connect::Connect;
+use hubcaps::issues::IssueOptions;
+use hubcaps::repositories::Repository;
 
 fn main () -> Result<()> {
     match env::var("INPUT_GITHUB-TOKEN").ok() {
@@ -13,7 +15,7 @@ fn main () -> Result<()> {
             let github = Github::new(
                 "ChildIssueAction",
                 Credentials::Token(token)
-            );
+            )?;
 
             create_issue(github)
         },
@@ -21,32 +23,88 @@ fn main () -> Result<()> {
     }
 }
 
-fn create_issue<C: Clone + Connect + 'static>(github: Github<C>) -> Result<()> {
+fn create_issue(github: Github) -> Result<()> {
     let org = env::var("INPUT_ORG").expect("GitHub org not provided");
     let repo = env::var("INPUT_PROJECT").expect("GitHub project not provided");
 
-    let issue = build_issue().expect("Failed to create Issue metadata");
-
     let mut runtime = Runtime::new()?;
+    let repo = github.repo(org, repo);
 
     runtime.block_on(
-        github.repo(org, repo)
-            .issues()
-            .create(&issue)
+        match build_issue(&repo).ok() {
+            Some(issue) => repo.issues().create(&issue),
+            None => panic!("Failed to build issue")
+        }
     )?;
 
     Ok(())
 }
 
-fn build_issue() -> Result<IssueOptions> {
-    let title = env::var("INPUT_TITLE").expect("Issue Title not set");
-    let body = env::var("INPUT_BODY").ok();
+fn build_issue(repo: &Repository) -> Result<IssueOptions> {
+    let mut subs = HashMap::new();
+    substitutions(&mut subs);
+    let title = env::var("INPUT_TITLE").expect("Issue Title is Required!");
     let assignee = env::var("INPUT_ASSIGNEE").ok();
     let milestone = match env::var("INPUT_MILESTONE").ok() {
         Some(milestone) => milestone.parse::<u64>().ok(),
         None => None
     };
-    let labels: Vec<String> = vec!();
 
-    Ok(IssueOptions::new(title, body, assignee, milestone, labels))
+    let issue = match env::var("INPUT_TEMPLATE").ok() {
+        Some(template) => {
+            let mut issue = template::process(repo, &template, subs);
+            issue.title = title;
+            issue.assignee = assignee;
+            issue.milestone = milestone;
+
+            issue
+        },
+        None => {
+            let body = env::var("INPUT_BODY").ok();
+            let labels: Vec<String> = vec!();
+            
+            IssueOptions::new(title, body, assignee, milestone, labels)
+        }
+    };
+
+    Ok(issue)
+}
+
+fn substitutions(subs: &mut HashMap<String, String>) {
+    for (key, value) in env::vars() {
+        match key.starts_with("INPUT_SUBSTITUTION") {
+            true => {
+                let mut match_parts: Vec<&str> = key.as_str().split('_').collect();
+
+                match match_parts.pop() {
+                    Some(target) => {
+                        subs.insert(target.to_string(), value);
+                    },
+                    None => ()
+                }
+            },
+            false => ()
+        }
+    }
+
+    ()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn setup_env() {
+        env::set_var("INPUT_SUBSTITUTION_TARGET", "value");
+    }
+
+    #[test]
+    fn substitution_test() {
+        setup_env();
+        let mut subs = HashMap::new();
+
+        substitutions(&mut subs);
+
+        assert_eq!(subs.get("TARGET"), Some(&"value".to_string()));
+    }
 }
---
+++ src/template.rs
@@ -0,0 +1,235 @@
+use hubcaps::content::File;
+use hubcaps::errors::Error;
+use hubcaps::issues::IssueOptions;
+use hubcaps::repositories::Repository;
+use std::collections::HashMap;
+use std::env;
+use tokio::runtime::Runtime;
+
+fn populate_metadata(issue: &mut IssueOptions, template: &mut str) {
+    let mut lines = template.lines();
+
+    lines.next(); // Swallow the leading `----`
+    lines.next(); // Swallow the name
+    lines.next(); // Swallow the about meta
+
+    match lines.next() {
+        Some(line) => {
+            match line.split(':').next_back() {
+                Some(title) => issue.title = title.trim_start().to_string(),
+                None => ()
+            }
+        },
+        None => ()
+    };
+
+    match lines.next() {
+        Some(line) => {
+            match line.split(':').next_back() {
+                Some(labels) => issue.labels = labels.trim_start().split(',').map(|tag| tag.to_string()).collect(),
+                None => ()
+            }
+        },
+        None => ()
+    };
+
+    match lines.next() {
+        Some(line) => {
+            match line.split(':').next_back() {
+                Some(assignees) => issue.assignee = Some(assignees.trim_start().to_string()),
+                None => ()
+            }
+        },
+        None => ()
+    };
+
+    lines.next(); // Swallow the blank line after the metadata
+    lines.next(); // Swallow the ---- line separating the metadata from the body
+    lines.next(); // Swallow the blank line at the top of the body
+
+    issue.body = Some(lines.collect::<Vec<&str>>().join("\n"));
+}
+
+fn fetch(repo: &Repository, name: &str) -> Result<File, Error> {
+    let mut runtime = Runtime::new()?;
+
+    runtime.block_on(
+        repo.content()
+            .file(name)
+    )
+}
+
+pub fn process(repo: &Repository, template_name: &str, subs: HashMap<String, String>) -> IssueOptions {
+    let title = match env::var("INPUT_TITLE").ok() {
+        Some(title) => title,
+        None => "".to_string()
+    };
+    let labels: Vec<String> = vec!();
+    let body: Option<String> = None;
+    let assignee: Option<String> = None;
+    let milestone: Option<u64> = None;
+
+    let mut issue = IssueOptions::new(title, body, assignee, milestone, labels);
+
+    match fetch(repo, template_name) {
+        Ok(file) => {
+            let template = std::str::from_utf8(file.content.as_ref()).expect("Non UTF8 contents in template");
+            populate_metadata(&mut issue, &mut template.to_string().clone());
+            match issue.body {
+                Some(mut content) => {
+                    issue.body = Some(substitute(&mut content, subs));
+                    ()
+                },
+                None => ()
+            }
+        },
+        Err(_) => panic!("Template could not be fetched!")
+    };
+
+    issue
+}
+
+fn substitute(body: &mut str, subs: HashMap<String, String>) -> String {
+    let mut subbed = body.to_string();
+
+    for (target, replacement) in subs.iter() {
+        subbed = subbed.replace(target, replacement);
+    }
+
+    subbed
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEMPLATE_NAME: &str = ".github/ISSUE_TEMPLATE/testing-template.md";
+
+    fn template() -> String {
+        let t = r#"---
+        name: Template
+        about: This is a template
+        title: Test title
+        labels: new,bug
+        assignees: ''
+
+        ----
+
+        # Sample Template
+        ## Subs
+        {{ first }}
+        {{ second }}
+        ## Other Stuff
+        Hi Mom!
+        "#;
+
+        t.to_string()
+    }
+
+    fn raw_body() -> String {
+        r#"# Sample Template
+        ## Subs
+        {{ first }}
+        {{ second }}
+        ## Other Stuff
+        Hi Mom!
+        "#.to_string()
+    }
+
+    fn subbed_body() -> String {
+        let t = r#"# Sample Template
+        ## Subs
+        FIRST
+        SECOND
+        ## Other Stuff
+        Hi Mom!
+        "#;
+
+        t.to_string()
+    }
+
+    fn partial_sub() -> String {
+        let t = r#"# Sample Template
+        ## Subs
+        FIRST
+        {{ second }}
+        ## Other Stuff
+        Hi Mom!
+        "#;
+
+        t.to_string()
+    }
+
+    #[test]
+    fn full_template_substitution() {
+        let mut subs = HashMap::new();
+
+        subs.insert("{{ first }}".to_string(), "FIRST".to_string());
+        subs.insert("{{ second }}".to_string(), "SECOND".to_string());
+
+        assert_eq!(subbed_body(), substitute(&mut raw_body(), subs));
+    }
+
+    #[test]
+    fn sub_without_matches() {
+        assert_eq!(raw_body(), substitute(&mut raw_body(), HashMap::new()));
+    }
+
+    #[test]
+    fn partial_substitution() {
+        let mut subs = HashMap::new();
+
+        subs.insert("{{ first }}".to_string(), "FIRST".to_string());
+
+        assert_eq!(partial_sub(), substitute(&mut raw_body(), subs));
+    }
+
+    #[test]
+    fn template_decomposition() {
+        let title = "";
+        let labels: Vec<String> = vec!();
+        let body: Option<String> = None;
+        let assignee: Option<String> = None;
+        let milestone: Option<u64> = None;
+        let mut issue = IssueOptions::new(title, body, assignee, milestone, labels);
+
+        populate_metadata(&mut issue, &mut template());
+
+        assert_eq!("Test title", issue.title);
+        assert_eq!(vec!["new", "bug"], issue.labels);
+        assert_eq!(Some(raw_body()), issue.body);
+    }
+
+    #[test]
+    fn template_fetch() {
+        let token = match std::env::var("AUTH_TOKEN").ok() {
+            Some(token) => hubcaps::Credentials::Token(token),
+            _ => panic!("No Auth Token Found")
+        };
+
+        let github = hubcaps::Github::new(
+            "ChildIssueActionTest",
+            token
+        );
+        
+        let repo = match github {
+            Ok(github) => github.repo("LeakyBucket", "child_issue"),
+            Err(_) => panic!("Couldn't build Github struct")
+        };
+
+        let file = match fetch(&repo, TEMPLATE_NAME) {
+            Ok(file) => file,
+            Err(error) => {
+                dbg!(error);
+                panic!("Download failed")
+            }
+        };
+
+        let contents = match std::str::from_utf8(file.content.as_ref()) {
+            Ok(content) => content,
+            Err(_) => panic!("Could not convert content")
+        };
+
+        assert!(contents.chars().count() > 0);
+    }
+}
